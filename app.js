94b0bc1737a35c84b106fa0d272b4234d8bbd9d1
Akash <akash@gmail.com>
2019-08-06 23:29:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,109 @@
+// Get the canvas and set its context --01
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// Set the canvas width and height equal to screen width and height --02
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
+// if this varables true you can draw on canvas 
+let isDraw = false;
+
+// Add events to the canvas --03
+canvas.addEventListener('mousedown', startDraw);
+canvas.addEventListener('mouseup', endDraw);
+canvas.addEventListener('mousemove', draw);
+
+// When mouse button down --04 (i)
+function startDraw() {
+    isDraw = true;
+}
+
+// when mouse button up --04 (ii)
+function endDraw() {
+    isDraw = false;
+    ctx.beginPath();
+    ctx.shadowBlur = 0;
+}
+
+// Variables
+let pencil = true;
+let brush = false;
+let eraser = false;
+
+// When mouse move and the varaible isDraw is true --05
+function draw(event) {
+    if (!isDraw) return;
+
+    if (pencil) {
+        let colorInput = document.getElementById('color').value;
+        let lineWidth = document.getElementById('size-input').value;
+        if (lineWidth <= 25)
+            ctx.lineWidth = lineWidth;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = colorInput;
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+
+    } else if (brush) {
+        let colorInput = document.getElementById('color').value;
+        let lineWidth = document.getElementById('size-input').value;
+        if (lineWidth <= 25)
+            ctx.lineWidth = lineWidth;
+        ctx.lineJoin = ctx.lineCap = 'round';
+        ctx.strokeStyle = colorInput;
+        ctx.shadowBlur = 5;
+        ctx.shadowColor = colorInput;
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+
+    } else if (eraser) {
+        let lineWidth = document.getElementById('size-input').value;
+        if (lineWidth <= 25)
+            ctx.lineWidth = lineWidth;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = '#fff';
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+    }
+}
+
+// Events to all tools buttons on the sidebar
+let tools = document.getElementsByClassName('tools');
+for (let index = 0; index < tools.length; index++) {
+    tools[index].addEventListener('click', () => {
+        let id = tools[index].getAttribute('id');
+        switch (id) {
+            case "pencil":
+                pencil = true;
+                brush = false;
+                eraser = false;
+                break;
+
+            case "brush":
+                brush = true;
+                pencil = false;
+                eraser = false;
+                break;
+
+            case "eraser":
+                eraser = true;
+                brush = false;
+                pencil = false;
+                break;
+        }
+    });
+}
+
+// When export button clicked download the canvas 
+document.getElementById('export').addEventListener('click', () => {
+    var dataURL = canvas.toDataURL();
+    document.getElementById('mirror').src = dataURL;
+    document.getElementById('export').href = dataURL;
+    document.getElementById('mirror').src = "";
+});
+
+// when clear button clicked clear the canvas by reloading page
+document.getElementById('clear').addEventListener('click', () => {
+    location.reload();
+});
